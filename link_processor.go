6d947d9114e475ad5289099352e55d20dcb587aa
SaltyFrappuccino <saltyfrappuccino@internet.ru>
2024-11-24 14:55:53 +0400

    Add docs comments for link_processor.go

--- link_processor.go
+++ link_processor.go
@@ -1,70 +1,58 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"net/url"
-	"os"
+	"log"
 	"path/filepath"
 	"strings"
 )
 
-func FindAndReadFile(filePath string) (string, error) {
-	if !filepath.IsAbs(filePath) {
-		cwd, err := os.Getwd()
+func main() {
+	pathFlag := flag.String("path", "", "Path to the file containing URLs (relative or absolute)")
+	sizeFlag := flag.String("size", "bytes", "Content size unit: bytes, kb, mb, chars")
+	saveFlag := flag.Bool("save", false, "Save results to a file (true/false)")
+
+	flag.Parse()
+
+	if *pathFlag == "" {
+		log.Fatal("The --path flag is required")
+	}
+
+	var sizeUnit SizeUnit
+	switch strings.ToLower(*sizeFlag) {
+	case "kb":
+		sizeUnit = KB
+	case "mb":
+		sizeUnit = MB
+	case "chars":
+		sizeUnit = Chars
+	case "bytes":
+		sizeUnit = Bytes
+	default:
+		log.Fatalf("Invalid value for --size: %s. Use bytes, kb, mb, or chars", *sizeFlag)
+	}
+
+	links, err := ProcessFile(*pathFlag)
+	if err != nil {
+		log.Fatalf("Error processing file: %v", err)
+	}
+	if len(links) == 0 {
+		log.Fatal("No valid URLs found in the file")
+	}
+	fmt.Println("URL processing completed.")
+
+	results := ProcessSites(links, sizeUnit)
+	fmt.Println("Site processing completed.")
+
+	DisplayResults(results, sizeUnit)
+
+	if *saveFlag {
+		outputFile := filepath.Join(filepath.Dir(*pathFlag), "results.txt")
+		err := SaveResultsToFile(results, sizeUnit, outputFile)
 		if err != nil {
-			return "", fmt.Errorf("error getting current working directory: %v", err)
+			log.Fatalf("Error saving results to file: %v", err)
 		}
-		filePath = filepath.Join(cwd, filePath)
+		fmt.Printf("Results have been saved to file: %s\n", outputFile)
 	}
-
-	content, err := os.ReadFile(filePath)
-	if err != nil {
-		return "", fmt.Errorf("error reading file: %v", err)
-	}
-
-	return string(content), nil
-}
-
-func ExtractAndNormalizeLinks(content string) []string {
-	var links []string
-	scanner := bufio.NewScanner(strings.NewReader(content))
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		words := strings.Fields(line)
-
-		for _, word := range words {
-			if link := normalizeLink(word); link != "" {
-				links = append(links, link)
-			}
-		}
-	}
-
-	return links
-}
-
-func normalizeLink(word string) string {
-	if !strings.HasPrefix(word, "http://") && !strings.HasPrefix(word, "https://") {
-		word = "https://" + word
-	}
-
-	u, err := url.Parse(word)
-	if err != nil || u.Hostname() == "" {
-		return ""
-	}
-
-	u.Scheme = "https"
-
-	return fmt.Sprintf("https://%s", u.Hostname())
-}
-
-func ProcessFile(filePath string) ([]string, error) {
-	content, err := FindAndReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	links := ExtractAndNormalizeLinks(content)
-	return links, nil
 }
